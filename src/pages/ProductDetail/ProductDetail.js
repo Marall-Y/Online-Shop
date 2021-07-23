20a8ad0bf7f1ed7a0850b2f631b18dca28811805
Morteza Shirbeigi <shirbeigi.morteza@gmail.com>
2021-07-23 19:19:03 +0430

    fix(productDetail): fix find product

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,15 +1,47 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const ProductDetail = (props) => {
-  const data = props.location.data;
+const ProductDetail = ({location, match}) => {
 
-  console.log(data);
+const [product, setProduct] = useState({})
 
-  const product = data.find((item) => {
-    console.log(item);
-  });
+// find product item form product list
+const findProduct = (ProductList) => {
+  const productItem = ProductList.find(item => item.id == match.params.id);
+  productItem && setProduct(productItem);
+}
 
-  return <div>hi</div>;
+useEffect(()=> {
+  // call api again on refresh page
+  if(location.data) {
+    findProduct(location.data)
+  } else {
+    //call api
+    axios
+      .get(
+        "https://online-shop-49336-default-rtdb.firebaseio.com/products.json"
+      )
+      .then((response) => {
+        const data = response.data;
+        findProduct(data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+},[])
+
+  return (
+    product.title 
+      ? (
+        <>
+          <h1>{product.id}</h1>
+          <h1>{product.title}</h1>
+          <h1>{product.price}</h1>
+        </>
+      )
+      : <h1>Loading</h1>
+  );
 };
 
 export default ProductDetail;
